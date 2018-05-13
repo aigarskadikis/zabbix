8954af18e79093bd6de8a05c7d7ff1dab6df0e0e
catonrug <aigars.kadikis@gmail.com>
2018-05-13 18:31:51 +0300

    next

--- conf.check.cmd
+++ conf.check.cmd
@@ -7,6 +7,8 @@ rem 1 - zabbix_agentd.conf.latest file is ready to initiate the original. We are
 rem 2 - the file online do not match with the checksum provided. This is serious error. Need to fix as soon as possible.
 rem 3 - the downloaded file now match the checksumm provided. The zabbix_agentd.conf.latest are ready to take the place of zabbix_agentd.conf. Waiting for {$REPLACE_AGENTD_CONF}=1 setting in host level
 rem 4 - no parameter was given to program
+rem 5 - everything is up to date but there are still 'zabbix_agentd.conf.latest' file on filesystem. let's delete it.
+
 
 rem if no parameter is given to this program the exit end return status code 1
 if "%1" == "" (
@@ -14,16 +16,16 @@ echo 4
 exit /b 4
 )
 rem check if active config match the checksum
-sha1sum "c:\zabbix\zabbix_agentd.conf" | grep "%1" > nul 2>&1
+sha1sum "%~dp0zabbix_agentd.conf" | grep "%1" > nul 2>&1
 
 rem if do not match the checksum then..
 if not !errorlevel!==0 (
 
 rem check if there is already downloaded file which match the checksum
-if exist "c:\zabbix\zabbix_agentd.conf.latest" (
+if exist "%~dp0zabbix_agentd.conf.latest" (
 
 rem make checksum
-sha1sum "c:\zabbix\zabbix_agentd.conf.latest" | grep "%1" > nul 2>&1
+sha1sum "%~dp0zabbix_agentd.conf.latest" | grep "%1" > nul 2>&1
 
 rem if checksum match then 'echo 1' and exit programm
 if !errorlevel!==0 (
@@ -32,25 +34,31 @@ exit /b 0
 )
 
 rem if the checksum did not match then delete the file
-del "c:\zabbix\zabbix_agentd.conf.latest" > nul 2>&1
+del "%~dp0zabbix_agentd.conf.latest" > nul 2>&1
 )
 
 rem if there is a need to download latest file which are suposed to match the checksumm
-if not exist "c:\zabbix\zabbix_agentd.conf.latest" (
+if not exist "%~dp0zabbix_agentd.conf.latest" (
 rem download file
-curl -s "https://raw.githubusercontent.com/catonrug/zabbix/master/zabbix_agentd.conf" > "c:\zabbix\zabbix_agentd.conf.latest"
+curl -s "https://raw.githubusercontent.com/catonrug/zabbix/master/zabbix_agentd.conf" > "%~dp0zabbix_agentd.conf.latest"
 
 rem check if the file online match the checksum
-sha1sum "c:\zabbix\zabbix_agentd.conf.latest" | grep "%1" > nul 2>&1
+sha1sum "%~dp0zabbix_agentd.conf.latest" | grep "%1" > nul 2>&1
 if not !errorlevel!==0 (
-del "c:\zabbix\zabbix_agentd.conf.latest" > nul 2>&1
+del "%~dp0zabbix_agentd.conf.latest" > nul 2>&1
 echo 2
 exit /b 2
 ) else echo 3
 
 )
 
+)
 
+rem everything is up to date already. let's clear 'zabbix_agentd.conf.latest' if it still exists
+if exist "%~dp0zabbix_agentd.conf.latest" (
+del "%~dp0zabbix_agentd.conf.latest" > nul 2>&1
+echo 5
+exit /b 5
 ) else echo 0
 
 endlocal
