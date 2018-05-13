c366429b434c1e57264371c4237573377d9b3fa5
catonrug <aigars.kadikis@gmail.com>
2018-05-13 13:43:03 +0300

    upgrade
    0 = local conf match the version on git
    101 = zabbix_agentd.conf.latast has been deleted because the hash did not match
    102 = nothing has been downloaded

--- conf.check.cmd
+++ conf.check.cmd
@@ -1,29 +1,25 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-if not "%1" == "" (
+if "%1" == "" exit /b 1
 
 sha1sum "c:\zabbix\zabbix_agentd.conf" | grep "%1" > nul 2>&1
 
 if not !errorlevel!==0 (
-curl -s "https://catonrug.blogspot.com/feeds/posts/default/6828580331777948799?alt=json" | sed "s/{\|.}/\n/g" | grep ":.html.," | sed "s/^.*\d034//g;s/\\\\//g" > "c:\zabbix\zabbix_agentd.conf.latest.base64"
 
-if exist "c:\zabbix\zabbix_agentd.conf.latest.base64" (
-start /wait "" "%systemroot%\System32\certutil.exe" -decode "c:\zabbix\zabbix_agentd.conf.latest.base64" "c:\zabbix\zabbix_agentd.conf.latest" > nul 2>&1
+curl -s "https://raw.githubusercontent.com/catonrug/zabbix/master/zabbix_agentd.conf" > "c:\zabbix\zabbix_agentd.conf.latest"
+
+if exist "c:\zabbix\zabbix_agentd.conf.latest" (
 
 sha1sum "c:\zabbix\zabbix_agentd.conf.latest" | grep "%1" > nul 2>&1
 if not !errorlevel!==0 del "c:\zabbix\zabbix_agentd.conf.latest" > nul 2>&1
 
 if exist "c:\zabbix\zabbix_agentd.conf.latest" (
 ls -s c:\zabbix\zabbix_agentd.conf.latest | sed "s/ .*$//g"
-) else echo 0
+) else echo 101
+
+) else echo 102
 
 ) else echo 0
-
-) else echo 0
-
-) else echo 0
-
-if exist "c:\zabbix\zabbix_agentd.conf.latest.base64" del "c:\zabbix\zabbix_agentd.conf.latest.base64" > nul 2>&1
 
 endlocal
