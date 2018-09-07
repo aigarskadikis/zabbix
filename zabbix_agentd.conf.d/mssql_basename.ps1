5cacd4a7d94e1fa3ba801156e787d89743bcfd69
catonrug <aigars.kadikis@gmail.com>
2018-09-07 18:07:30 +0300

    next

--- zabbix_agentd.conf.d/mssql_basename.ps1
+++ zabbix_agentd.conf.d/mssql_basename.ps1
@@ -1,4 +1,4 @@
-﻿#функция для приведения к формату который понимает zabbix / the function is to bring to the format understands zabbix
+﻿#the function is to bring to the format understands zabbix
 function convertto-encoding ([string]$from, [string]$to){
     begin{
         $encfrom = [system.text.encoding]::getencoding($from)
@@ -11,24 +11,24 @@ function convertto-encoding ([string]$from, [string]$to){
     }
 }
 
-#Задаем переменные для подключение к MSSQL. $uid и $pwd нужны для проверки подлинности windows / We define the variables for connecting to MS SQL. $uid и $pwd need to authenticate windows
+#We define the variables for connecting to MS SQL. $uid nad $pwd need to authenticate windows
 $SQLServer = $(hostname.exe)
 #$uid = "Login" 
 #$pwd = "Password"
 
-#Создаем подключение к MSSQL / Create a connection to MSSQL
+#Create a connection to MSSQL
 
-#Если проверка подлинности windows / If windows authentication
+#If windows authentication
 #$connectionString = "Server = $SQLServer; User ID = $uid; Password = $pwd;"
 
-#Если Интегрированная проверка подлинности / If integrated authentication
+#If integrated authentication
 $connectionString = "Server = $SQLServer; Integrated Security = True;"
 
 $connection = New-Object System.Data.SqlClient.SqlConnection
 $connection.ConnectionString = $connectionString
 $connection.Open()
 
-#Создаем запрос непосредственно к MSSQL / Create a request directly to MSSQL
+#Create a request directly to MSSQL
 $SqlCmd = New-Object System.Data.SqlClient.SqlCommand  
 $SqlCmd.CommandText = "SELECT name FROM  sysdatabases"
 $SqlCmd.Connection = $Connection
@@ -38,10 +38,10 @@ $DataSet = New-Object System.Data.DataSet
 $SqlAdapter.Fill($DataSet) > $null
 $Connection.Close()
 
-#Получили список баз. Записываем в переменную. / We get a list of databases. Write to the variable.
+#We get a list of databases. Write to the variable.
 $basename = $DataSet.Tables[0]
 
-#Парсим и передаем список баз в zabbix. В последней строке нужно вывести имя бызы без запятой в конце. / Parse and pass a list of databases in zabbix. In the last line need to display the database name without a comma at the end.
+#Parse and pass a list of databases in zabbix. In the last line need to display the database name without a comma at the end.
 $idx = 1
 write-host "{"
 write-host " `"data`":[`n"
