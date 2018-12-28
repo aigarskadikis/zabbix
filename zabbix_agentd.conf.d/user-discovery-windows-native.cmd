6a32b44909a6a1ae7ddc49312489746ce845b350
catonrug <aigars.kadikis@gmail.com>
2018-12-28 10:53:37 +0200

    next

--- zabbix_agentd.conf.d/user-discovery-windows-native.cmd
+++ zabbix_agentd.conf.d/user-discovery-windows-native.cmd
@@ -1,7 +1,5 @@
 @echo off
 rem windows user discover
-if exist "c:\users" set dir=c:\users
-if not exist "c:\users" set dir=c:\documents and settings
 echo {"data":[
-for /F "tokens=*" %%a in ('dir /b "%dir%"') do echo {"{#USERNAME}":"%%a"},
+for /F "tokens=*" %%a in ('dir /b "%1"') do echo {"{#USERNAME}":"%%a"},
 echo {"{#USERNAME}":"XqMzJztwPV6hybmfteJj"}]}
