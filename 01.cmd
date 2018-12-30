e73d07039649ced373bd4d79e437ce66b9e74dab
catonrug <aigars.kadikis@gmail.com>
2018-12-30 20:03:16 +0200

    split into modules

--- 01.cmd
+++ 01.cmd
@@ -1,21 +1,14 @@
 @echo off
+
+rem allow to use grep, zabbix_sender from current dir
 set path=%path%;%~dp0
+
+rem import %Server% variable, so zabbix_sender know what to contact
+call "%~dp0contact.cmd"
+
 setlocal EnableDelayedExpansion
 
-rem get zabbix the IP address of zabbix proxy/server
-for /f "tokens=*" %%s in ('^
-grep "^Server=" "%~dp0zabbix_agentd.conf.d\Server.conf" ^|
-grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" ^|
-grep -v "127.0.0.1"') do (
-
-echo IP address of zabbix proxy/server = %%s
-
-zabbix_sender -z %%s -s "%computername%" -k active.presenter.status -o 2
-
-
-
-)
-
+zabbix_sender -z %Server% -s "%computername%" -k active.presenter.status -o 0
 
 
 endlocal
---
+++ contact.cmd
@@ -0,0 +1,8 @@
+rem get zabbix the IP address of zabbix proxy/server
+for /f "tokens=*" %%s in ('^
+grep "^Server=" "%~dp0zabbix_agentd.conf.d\Server.conf" ^|
+grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" ^|
+grep -v "127.0.0.1"') do (
+set Server=%%s
+)
+echo IP address of zabbix proxy/server = %Server%
