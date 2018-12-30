b5f62500ff87f56fea7c84b5bd6e447216b14791
catonrug <aigars.kadikis@gmail.com>
2018-12-30 20:43:19 +0200

    one object

--- app2maintain.cmd
+++ app2maintain.cmd
@@ -1,12 +1,6 @@
 @echo off
 
-echo {"data":[
-echo {"{#NAME}":%1},
-echo {"{#UKEY}":%2},
-echo {"{#URL}":%3},
-echo {"{#CHECHSUM}":%4},
-echo {"{#METHOD}":%5}
-echo ]}
+echo {"data":[{"{#NAME}":%1,"{#UKEY}":%2,"{#URL}":%3,"{#CHECHSUM}":%4,"{#METHOD}":%5}]}
 
 rem test
 rem app2maintain.cmd "ActivePresenter" "ActivePresenter" "https://cdn.atomisystems.com/apdownloads/ActivePresenter_v7.5.2_setup.exe" "92d4ecd25f821df9dfb32c1140446421decf5bac" "01"
