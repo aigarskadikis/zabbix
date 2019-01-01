f25ef59b3ece095b017eada4e24489eae50fd6d9
catonrug <aigars.kadikis@gmail.com>
2019-01-01 11:33:43 +0200

    next

--- scenario.cmd
+++ scenario.cmd
@@ -1,11 +1,16 @@
 @echo off
-set %path%;%~dp0
+set path=%path%;%~dp0
 set o=%1
 echo %o%
-if not exist "%~dp0ccmcache" md "%~dp0ccmcache"
+
+for /f "tokens=*" %%l in ('curl -s %o% ^|
+jq -r ".entry|.id|.\"$t\"" ^|
+sed "s/^.*blog.//;s/.post./\\/"') do set d=%~dp0ccmcache\%%l
+if not exist "%d%" md "%d%"
+
 for /f "tokens=*" %%t in ('curl -s %o% ^| jq -r ".entry|.title|.\"$t\""') do set filename=%%t
 echo downloading %filename%
-curl -s %o% | jq -r ".entry|.content|.\"$t\"" > c:\zabbix\file.%filename%.base64 
-echo decoding 
-"%systemroot%\System32\certutil.exe" -decode "c:\zabbix\file.%filename%.base64" c:\zabbix\file.%filename%
-
+curl -s %o% | jq -r ".entry|.content|.\"$t\"" > "%d%\%filename%.base64"
+echo decoding
+if exist "%d%\%filename%" del "%d%\%filename%"
+"%systemroot%\System32\certutil.exe" -decode "%d%\%filename%.base64" "%d%\%filename%"
