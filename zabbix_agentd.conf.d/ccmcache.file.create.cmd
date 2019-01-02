9d363395f47945eb36dd0317a8b4d1249d29f225
catonrug <aigars.kadikis@gmail.com>
2019-01-02 07:28:05 +0200

    next

---
+++ zabbix_agentd.conf.d/ccmcache.file.create.cmd
@@ -0,0 +1,19 @@
+@echo off
+set path=%path%;%~dp0
+set z=c:\zabbix
+set o=%1
+echo %o%
+
+for /f "tokens=*" %%l in ('curl -s %o% ^|
+jq -r ".entry|.id|.\"$t\"" ^|
+sed "s/^.*blog.//" ^|
+sed "s/.post./\\\\/"') do set d=%z%\ccmcache\%%l
+echo "%d%"
+if not exist "%d%" md "%d%"
+
+for /f "tokens=*" %%t in ('curl -s %o% ^| jq -r ".entry|.title|.\"$t\""') do set filename=%%t
+echo downloading %filename%
+curl -s %o% | jq -r ".entry|.content|.\"$t\"" > "%d%\%filename%.base64"
+echo decoding
+if exist "%d%\%filename%" del "%d%\%filename%"
+"%systemroot%\System32\certutil.exe" -decode "%d%\%filename%.base64" "%d%\%filename%"
