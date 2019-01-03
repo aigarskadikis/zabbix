bf1acd086b5d61b65654de21a195ffd6057bc281
catonrug <aigars.kadikis@gmail.com>
2019-01-03 06:55:13 +0200

    works

--- zabbix_agentd.conf.d/ccmcache.if.cmd
+++ zabbix_agentd.conf.d/ccmcache.if.cmd
@@ -4,18 +4,21 @@ set z=c:\zabbix
 set c=%z%\ccmcache
 
 if exist "%c%" (
-rem report directory cound
+rem report directory count
 dir /b "%c%" | find /v /c ""
 
+rem list blog IDs
 for /f "tokens=*" %%l in ('dir /b "%c%"') do (
-echo %%l
 
+rem list post IDs
 for /f "tokens=*" %%b in ('dir /b "%c%\%%l"') do (
 
+rem check if source has been already prepared
 if not exist "%c%\%%l\%%b\offline.ready" (
 
+rem if source in not ready then shedule to prepare it IN BACKGROUND and EXIT PROGRAM
 for /f "tokens=*" %%p in ('dir /b "%c%\%%l\%%b\*.cmd"') do (
-echo %%p
+start "" "%c%\%%l\%%b\%%p" prepare
 )
 
 )
