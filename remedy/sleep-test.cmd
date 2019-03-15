6c11c326cd10d4badf88f7fc1cd86737192e768b
catonrug <aigars.kadikis@gmail.com>
2019-03-15 16:57:45 +0200

    next

---
+++ remedy/sleep-test.cmd
@@ -0,0 +1,10 @@
+@echo off
+echo 1 > C:\zabbix\remedy\sleep.log
+C:\zabbix\sleep.exe 120
+echo 2 >> C:\zabbix\remedy\sleep.log
+C:\zabbix\sleep.exe 120
+echo 3 >> C:\zabbix\remedy\sleep.log
+C:\zabbix\sleep.exe 120
+echo 4 >> C:\zabbix\remedy\sleep.log
+C:\zabbix\sleep.exe 120
+echo 5 >> C:\zabbix\remedy\sleep.log
