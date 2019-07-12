3be2a0f2f1b56f2685b829f75cb25470f272b812
catonrug <aigars.kadikis@gmail.com>
2019-07-12 13:17:34 +0300

    show ISP

--- app2maintain.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo {"data":[{"{#NAME}":%1,"{#UKEY}":%2,"{#URL}":%3,"{#CHECHSUM}":%4,"{#METHOD}":%5}]}
-
-rem test
-rem app2maintain.cmd "ActivePresenter" "ActivePresenter" "https://cdn.atomisystems.com/apdownloads/ActivePresenter_v7.5.2_setup.exe" "92d4ecd25f821df9dfb32c1140446421decf5bac" "01"
--- contact.cmd
+++
@@ -1,8 +0,0 @@
-rem get zabbix the IP address of zabbix proxy/server
-for /f "tokens=*" %%s in ('^
-grep "^Server=" "%~dp0zabbix_agentd.conf.d\Server.conf" ^|
-grep -E -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" ^|
-grep -v "127.0.0.1"') do (
-set Server=%%s
-)
-echo %Server%
